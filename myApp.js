0805b71ee63b603d1506552ec5a0aac57a0ffbd2
AleMMb <alejandramellab@gmail.com>
2025-10-10 10:14:46 -0300

    Get Route Parameter Input from the Client Challenge

--- myApp.js
+++ myApp.js
@@ -20,6 +20,11 @@ app.get("/now", function(req, res, next) {
 });
 
 
+app.get("/:word/echo", function(req, res) {
+  res.json({echo: req.params.word});
+});
+
+
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname + "/views/index.html"));
 });
